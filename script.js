dfba56fb109a6469f0e20c13e1ece574a6bbfff2
Reshetnikov Dmitriy <Reshetnikov.111@mail.ru>
2023-10-30 15:15:40 +0300

    выполнил урок 2

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'chitaiMoskva';
+let screens = 'Simple, Heard, Interactive';
+let screenPrice = 1000;
+let rollback = 99;
+let fullPrice = 30000;
+let adaptive = true;
 
-alert('Привет, меня зовут Дима, а тебя?');
+//lesson01
+/*alert('Привет, меня зовут Дима, а тебя?');
 prompt('Смелее вводи своё имя, познакомимся поближе!');
-console.log('задание 1 выполнено');
+console.log('задание 1 выполнено');*/
+
+//lesson02
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice);
+console.log(fullPrice);
+console.log(screens.toLowerCase());
+console.log(screens.split(" "));
+console.log((fullPrice * (rollback/100)));
+
+
+
+
