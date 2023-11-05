b63f1a193150dfc0070e142d3f28df1fc00152ee
Reshetnikov Dmitriy <Reshetnikov.111@mail.ru>
2023-11-05 11:08:05 +0300

    lesson04

--- script.js
+++ script.js
@@ -1,90 +1,68 @@
 'use strict';
-// let title = prompt('Как называется ваш проект?');
-// let screens = prompt('Какие типы экранов нужно разработать?', "Simple, Heard, Interactive");
-// let screenPrice = prompt('Сколько будет стоить данная работа?', "12000");
-// let rollback = 12;
-// let adaptive = confirm("Нужен ли адаптив на сайте?");
-// let service1 = prompt("Какой дополнительный тип услуги нужен?");
-// let servicePrice1 = prompt("Сколько это будет стоить?");
-// let service2 = prompt("Какой дополнительный тип услуги нужен?");
-// let servicePrice2 = prompt("Сколько это будет стоить?");
-// let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-// let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
-// switch (true) {
-// 	case fullPrice >= 30000:
-// 		alert("Даем скидку в 10%");
-// 		console.log("Даем скидку в 10%");
-// 		break
-// 	case fullPrice >= 15000 && fullPrice < 30000:
-// 		alert("Даем скидку в 5%");
-// 		console.log("Даем скидку в 5%");
-// 		break
-// 	case fullPrice >= 0 && fullPrice < 15000:
-// 		alert("Скидка не предусмотрена");
-// 		console.log("Скидка не предусмотрена");
-// 		break
-// 	default:
-// 		alert("Что то пошло не так");
-// 		console.log("Что то пошло не так");
-// };
+let service1 = prompt("Какой дополнительный тип услуг нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуг нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 
-//lesson01
-// alert('Привет, меня зовут Дима, а тебя?');
-// prompt('Смелее вводи своё имя, познакомимся поближе!');
-// console.log('задание 1 выполнено');
+let rollback = 12;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-//lesson02
-// console.log(typeof title);
-// console.log(typeof fullPrice);
-// console.log(typeof adaptive);
-// console.log(screens.length);
-// console.log(screenPrice);
-// console.log(fullPrice);
-// console.log(screens.toLowerCase());
-// console.log(screens.split(" "));
-// console.log((fullPrice * (rollback/100)));
+const getAllServicePrices = function () {
+    return servicePrice1 + servicePrice2;
+};
 
-//lesson03
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+};
 
-// console.log(title);
-// console.log(screens);
-// console.log(service1);
-// console.log(servicePrice1);
-// console.log(service2);
-// console.log(servicePrice2);
-// console.log("Откат посреднику", Math.floor(servicePercentPrice));
+const getFullPrice = function () {
+    return screenPrice + allServicePrices;
+};
 
-								//hard
-// let lang = 'ru';
+const getServicePercentPrice = function () {
+    return fullPrice - (fullPrice * (rollback / 100));
+};
 
-// if (lang = 'ru') {
-// 	console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
-// } else if (lang = 'en') {
-// 	console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-// } else {
-// 	console.log('неопределённое значение');
-// };
+const getTitle = function () {
+    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLocaleLowerCase();
+};
 
-// switch (true) {
-// 	case lang = 'ru':
-// 		console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
-// 		break;
-// 	case lang = 'en':
-// 		console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-// 		break;
-// 	default:
-// 		console.log('неопределённое значение');
-// };
+const getRollbackMessage = function (price) {
+    if (price >= 30000) {
+        return "Даем скидку в 10%";
+    } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5%";
+    } else if (price >= 0 && price < 15000) {
+        return "Скидка не предусмотрена";
+    } else {
+        return "Что то пошло не так";
+    };
+};
 
-// const langArray = {
-// 	ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-// 	en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-// };
-// console.log(langArray[lang]);
+allServicePrices = getAllServicePrices();
+fullPrice = getAllServicePrices();
+servicePercentPrice = getAllServicePrices();
+title = getTitle();
 
-// let namePerson = '';
-// console.log((namePerson === 'Артём') ? 'директор' : (namePerson === 'Максим') ? 'преподаватель' : 'студент');
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
 
+console.log(getRollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log("Стоимость верстки экранов" + screenPrice + "рубли и Стоимость разработки сайта" + fullPrice + "рубли");
 
 
