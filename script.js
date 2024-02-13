88ac35e9595553022e0015cb8d9ebcfd152f362d
Reshetnikov Dmitriy <Reshetnikov.111@mail.ru>
2024-02-13 16:08:44 +0300

    добавил названия переменным

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Glo Academy';
+let screens = 'simple, difficult, interactive';
+let screenPrice = 1000;
+let rollback = 10;
+let fullPrice = 10000;
+let adaptive = true;
 
 alert('Привет GLO ACADEMY');
 console.log('задание 1 выполнено');
