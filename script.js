bbb60ab7007c3ebbc1370c9a58f1cec2f39170f2
Reshetnikov Dmitriy <Reshetnikov.111@mail.ru>
2024-02-13 17:40:43 +0300

    вывел в консоль переменные используя методы

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ let rollback = 10;
 let fullPrice = 10000;
 let adaptive = true;
 
-alert('Привет GLO ACADEMY');
+//alert('Привет GLO ACADEMY');
 console.log('задание 1 выполнено');
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(typeof title);
+console.log(screens.length);
+console.log(screenPrice);
+console.log(fullPrice);
+console.log(screens.toLowerCase());//console.log(screens.toLowerCase().split(", "));
+console.log(screens.split());
+console.log(fullPrice * (rollback/100));//1000
