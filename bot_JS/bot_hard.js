0cb1882311d85c1107d4c7b0b0f0e9e76dd98e5d
Reshetnikov Dmitriy <Reshetnikov.111@mail.ru>
2024-02-22 11:29:09 +0300

    добавил количество попыток боту

--- bot_JS/bot_hard.js
+++ bot_JS/bot_hard.js
@@ -9,33 +9,46 @@ const isNum = function (n) {
 
 const start = function () {
 	let numberR = getRandomInt(100);
+	console.log('numberR: ', numberR);
+	let attempts = 10;
 	const game = function () {
-		const num = prompt('"Угадай число от 1 до 100" (Для выхода оставьте строку пустой)');
-		if (num === null) {
-			alert('До свидания');
-			game();
-		}
-		if (isNum(num)) {
-			const realNum = +num;
-			if (realNum > numberR) {
-				alert('Загаданное число меньше');
-				game();
-			} else if (realNum < numberR) {
-				alert('Загаданное число больше');
-				game();
+		attempts--;
+		if (attempts < 0) {
+			if (confirm('Попытки закончились, хотите сыграть еще?')) {
+				start();
 			} else {
-				if (confirm('Вы угадали! Сыграем ещё?')) {
-					start();
-				} else {
-					alert('До свидания');
-					return;
-				}
-			}
+				alert('До свидания');
+				return;
+			};
 		} else {
-			alert('Введите число');
-			game();
-		}
+			const num = prompt('"Угадай число от 1 до 100" (Для выхода оставьте строку пустой)');
+			if (num === null) {
+				alert('До свидания');
+				game();
+			};
+			if (isNum(num)) {
+				const realNum = +num;
+				if (realNum > numberR) {
+					alert(`Загаданное число меньше, осталось попыток: ${attempts}`);
+					game();
+				} else if (realNum < numberR) {
+					alert(`Загаданное число больше, осталось попыток: ${attempts}`);
+					game();
+				} else {
+					if (confirm('Вы угадали! Сыграем ещё?')) {
+						start();
+					} else {
+						alert('До свидания');
+						return;
+					}
+				};
+			} else {
+				alert('Введите число');
+				game();
+			};
+		};
 	};
+	console.dir(game);
 	game();
 };
 
